112d524ca82982fa574aa01246364bdc5dbf7b10
DNA.h <s.n.bahador@gmail.com>
2020-03-23 13:45:15 +0430

    -now is a country guessing word game

--- project.py
+++ project.py
@@ -3,34 +3,43 @@ import random
 
 u = random.randint(0, len(read_words.read_secret()) - 1)
 secret = read_words.read_secret()[u]
+chances = []
+for x in range(0, len(secret)):
+    chances.append("*")
 right_guess = []
 secret_list = []
 characer_entered = []
 
-
 for x in range(0, len(secret)):
     secret_list.append("_")
 print(secret_list)
-n = 20
+print (chances)
+n = len(secret)
 flag = True
 while flag:
-    guessed_word = input(print("guess the word !"))
+    guessed_word = input("guess the word !\n")
     if guessed_word not in characer_entered:
-        characer_entered.append(guessed_word)
-        if len(guessed_word) > 1 :
+        if len(guessed_word) > 1:
             print("your guess must be only 1 character at a time")
             continue
-        n -= 1
-        print("you have {} chance left ", n)
 
+        characer_entered.append(guessed_word)
+
+        found = False
         for i in range(0, len(secret)):
             if secret[i] == guessed_word.lower():
+                found = True
                 secret_list[i] = guessed_word
                 right_guess.append(guessed_word)
 
-        print(secret_list)
+        if not found:
+            chances[len(secret) - n] = guessed_word
+            n -= 1
 
-        if n < 0:
+        print(secret_list)
+        print(chances)
+
+        if n <= 0:
             flag = False
             break
         if len(secret) == len(right_guess):
--- read_words.py
+++ read_words.py
@@ -1,6 +1,6 @@
 def read_secret():
     my_text = open("words.txt", "r")
-    my_list = my_text.read().split(",")
-    return my_list
+    my_list = my_text.read().split("\n")
     my_text.close()
+    return my_list
 
